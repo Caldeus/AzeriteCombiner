5b3d7dee0ed6e8c199b5f93754dcb11cdef5e618
Caldeus <41150508+Caldeus@users.noreply.github.com>
2019-10-26 17:20:42 +0200

    Update and rename PH to data.js

---
+++ js/data.js
@@ -0,0 +1,99 @@
+// classes with their respective specialications
+var classes = [
+    {
+        "name": "Death Knight",
+        "specs": [
+            specs.deathknightBlood,
+            specs.deathknightFrost,
+            specs.deathknightUnholy
+        ]
+    },
+        {
+        "name": "Demon Hunter",
+        "specs": [
+            specs.demonhunterHavoc,
+            specs.demonhunterVengeance,
+        ]
+    },
+        {
+        "name": "Druid",
+        "specs": [
+            specs.druidBalance,
+            specs.druidFeral,
+            specs.druidGuardian,
+            specs.druidRestoration
+        ]
+    },
+        {
+        "name": "Hunter",
+        "specs": [
+            specs.hunterBeastMastery,
+            specs.hunterMarksmanship,
+            specs.hunterSurvival
+        ]
+    },
+        {
+        "name": "Mage",
+        "specs": [
+            specs.mageArcane,
+            specs.mageFire,
+            specs.mageFrost
+        ]
+    },
+        {
+        "name": "Monk",
+        "specs": [
+            specs.monkBrewmaster,
+            specs.monkMistweaver,
+            specsmonkWindwalker.
+        ]
+    },
+        {
+        "name": "Paladin",
+        "specs": [
+            specs.paladinHoly,
+            specs.paladinProtection,
+            specs.paladinRetribution
+        ]
+    },
+        {
+        "name": "Priest",
+        "specs": [
+            specs.priestDiscipline,
+            specs.priestHoly,
+            specs.priestShadow
+        ]
+    },
+        {
+        "name": "Rogue",
+        "specs": [
+            specs.rogueAssassination,
+            specs.rogueOutlaw,
+            specs.rogueSubtlety
+        ]
+    },
+        {
+        "name": "Shaman",
+        "specs": [
+            specs.shamanElemental,
+            specs.shamanEnhancement,
+            specs.shamanRestoration
+        ]
+    },
+        {
+        "name": "Warlock",
+        "specs": [
+            specs.warlockAffliction,
+            specs.warlockDemonology,
+            specs.warlockDestruction
+        ]
+    },
+        {
+        "name": "Warrior",
+        "specs": [
+            specs.warriorArms,
+            specs.warriorFury,
+            specs.warriorProtection
+        ]
+    }
+];
