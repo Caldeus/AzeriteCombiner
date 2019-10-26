9b06704033d99e5286bd32d91eba762aceaf93a8
Caldeus <41150508+Caldeus@users.noreply.github.com>
2019-10-26 18:00:07 +0200

    Update and rename azeriteTraits to azeriteTraits.js

---
+++ js/azeriteTraits.js
@@ -0,0 +1,23 @@
+// 8.3 Raid Azerite Traits (Major)
+var traitsRaid = {
+  "ph1": {
+    "name": "PH1",
+    "url": "",
+    "img": "",
+  },
+  "ph2": {
+    "name": "PH2",
+    "url": "",
+    "img": "",
+  },
+  "ph3": {
+    "name": "PH3",
+    "url": "",
+    "img": "",
+  },
+};
+
+// Major Azerite Traits a Spec
+
+//
+//
