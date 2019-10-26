18bbe0b4f8306548302dece2798486c86f953091
Caldeus <41150508+Caldeus@users.noreply.github.com>
2019-10-26 18:55:13 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,43 @@
 
+
+
+
+function updateData() {
+  options.class = document.getElementById('select_class').value;
+  if (options.class == 0) {
+    
+  }
+  else if (options.class == 1) {
+    
+  }
+  else if (options.class == 2) {
+    
+  }
+  else if (options.class == 3) {
+    
+  }
+  else if (options.class == 4) {
+    
+  }
+  else if (options.class == 5) {
+    
+  }
+  else if (options.class == 6) {
+    
+  }
+  else if (options.class == 7) {
+    
+  }
+  else if (options.class == 8) {
+    
+  }
+  else if (options.class == 9) {
+    
+  }
+  else if (options.class == 10) {
+    
+  }
+  else if (options.class = 11) {
+    
+  }
+};
