b5213d6088daad89f0910172ba8b70612c7380f0
Caldeus <41150508+Caldeus@users.noreply.github.com>
2019-10-26 18:17:08 +0200

    Update and rename azeritePieces to azeritePieces.js

---
+++ js/azeritePieces.js
@@ -0,0 +1,13 @@
+// 8.3 Raid Azerite Pieces
+var piecesClothRaid = {
+  "ph1": {
+    "name": "PH Cloth Head 1",
+    "url": "",
+    "img": "",
+    "armorType": "Cloth",
+    "slot": "Head",
+    "location": "x. Bossname",
+    "azeriteTraits": [
+      traitsRaid.ph1
+  },
+};
