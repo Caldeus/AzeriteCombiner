1344ebb9e4c82ba8f84fdd2eda81d8f58ccc6c65
Caldeus <41150508+Caldeus@users.noreply.github.com>
2019-10-28 13:45:01 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,4 +3,9 @@ function updateSpec() {
   selectspec = document.getElementById('select_spec');
   
   selectspec.length = 0;
+  
+  if (selectclass.text == "Death Knight") {
+    selectspec.options.add("Blood", 0);
+  }
+  
 }
