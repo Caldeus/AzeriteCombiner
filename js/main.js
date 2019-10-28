bba260b1d4cd0a2b74763e4ec074d437074a831f
Caldeus <41150508+Caldeus@users.noreply.github.com>
2019-10-28 13:28:21 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,11 +1,7 @@
 function updateSpec() {
-  options.class = document.getElementById('select_class');
-  options.spec = document.getElementById('select_spec');
+  selectclass = document.getElementById('select_class');
+  selectspec = document.getElementById('select_spec');
   
-
-  options.spec.options.remove(0);
-  options.spec.options.remove(1);
-  options.spec.options.remove(2);
-  options.spec.options.remove(3);
+  selectspec.length = 0
 
  }
