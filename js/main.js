5f298b37ec3ec10f374faefa8dec8402a4a14ff3
Caldeus <41150508+Caldeus@users.noreply.github.com>
2019-10-28 14:20:00 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,20 +1,20 @@
 function updateSpec() {
   selectclass = document.getElementById('select_class');
   selectspec = document.getElementById('select_spec');
-  
-  selectspec.length = 0;
-  
-  var spec1 = document.createElement("spec1");
-  var spec2 = document.createElement("spec2");
-  var spec3 = document.createElement("spec3");
-  
-  if (selectclass.text == "Death Knight") {
-    spec1.text = "Blood";
-    spec1.text = "Frost";
-    spec1.text = "Unholy";
-    selectspec.options.add(spec1, 0);
-    selectspec.options.add(spec2, 1);
-    selectspec.options.add(spec3, 2);
+  if (selectspec.length > 0) {
+    selectspec.length = 0;
   }
+  
+  var spec1 = document.createElement("option");
+  var spec2 = document.createElement("option");
+  var spec3 = document.createElement("option");
+  
+  spec1.txt = "a";
+  spec2.txt = "b";
+  spec3.txt = "c";
+  
+  selectspec.options.add(spec1);
+  selectspec.options.add(spec2);
+  selectspec.options.add(spec3);
   
 }
