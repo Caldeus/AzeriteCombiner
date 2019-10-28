1c4954a9b5b5581dbc5fd2cc5d0798345a050415
Caldeus <41150508+Caldeus@users.noreply.github.com>
2019-10-28 13:12:46 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,58 +1,8 @@
-
-
-
-
 function updateSpec() {
-  options.class = document.getElementById('select_class').;
+  options.class = document.getElementById('select_class');
   options.spec = document.getElementById('select_spec');
   
   for (i = 0; i < options.spec.length; i++) {
-    options.spec.remove(i)
-  }
-  
-  //DK 0, DH 1, Dr 2, Hu 3, Ma 4, Mo 5, Pa 6, Pr 7, Ro 8, Sh 9, 10 WL, 11 War
-  if (options.class.text == 'Death Knight') {
-    
-    options.spec.options[options.spec.options.length] = new Option('Blood', '0', false, false);
-    options.spec.options[options.spec.options.length] = new Option('Frost', '0', false, false);
-    options.spec.options[options.spec.options.length] = new Option('Unholy', '0', false, false);
-  }
-  else if (options.class.text == 'Demon Hunter') {
-    options.spec.options[options.spec.options.length] = new Option('Havoc', '0', false, false);
-    options.spec.options[options.spec.options.length] = new Option('Vengeance', '0', false, false);
-  }
-  else if (options.class == 2) {
-    
-  }
-  else if (options.class == 3) {
-    
-  }
-  else if (options.class == 4) {
-    
-  }
-  else if (options.class == 5) {
-    
-  }
-  else if (options.class == 6) {
-    
-  }
-  else if (options.class == 7) {
-    
-  }
-  else if (options.class == 8) {
-    
-  }
-  else if (options.class == 9) {
-    
-  }
-  else if (options.class == 10) {
-    
-  }
-  else if (options.class = 11) {
-    
-  }
-}
-
-function rebuild() {
-  updateSpec();
-}
+    options.spec.remove(i);
+  }   
+ }
