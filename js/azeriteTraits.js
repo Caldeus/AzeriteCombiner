805f45b30ea0a6dd79b3d3d68853499452fdb41c
Caldeus <41150508+Caldeus@users.noreply.github.com>
2019-11-05 10:44:18 +0100

    Update azeriteTraits.js

--- js/azeriteTraits.js
+++ js/azeriteTraits.js
@@ -1,23 +1,53 @@
-// 8.3 Raid Azerite Traits (Major)
+// 8.3 Raid Azerite Traits (T3)
 var traitsRaid = {
   "ph1": {
     "name": "PH1",
     "url": "",
     "img": "",
+    "description": "",
   },
   "ph2": {
     "name": "PH2",
     "url": "",
     "img": "",
+    "description": "",
   },
   "ph3": {
     "name": "PH3",
     "url": "",
     "img": "",
+    "description": "",
   }
 };
 
-// Major Azerite Traits a Spec
-
-//
-//
+// Major Class Azerite Traits (T4/T3)
+var traitsDK = {
+  "bloodyruneblade": {
+    "name": "Bloody Runeblade",
+    "url": "https://www.wowhead.com/spell=289339",
+    "img": "inv_sword_2h_blood_c_02.png",
+    "description": "When Crimson Scourge activates, you gain x Haste for 5 sec, and immediately gain 5 Runic Power.",
+  },
+  "bonespikegraveyard": {
+    "name": "Bone Spike Graveyard",
+    "url": "https://www.wowhead.com/spell=273088",
+    "img": "",
+    "description": "Casting Death and Decay impales enemies with bone spikes, inflicting x Physical damage and healing you for y per enemy.",
+  },
+  "bonesofthedamned": {
+    "name": "Bones of the Damned",
+    "url": "https://www.wowhead.com/spell=278484",
+    "img": "",
+    "description": "Marrowrend has a chance to grant an extra charge of Bone Shield. Bone Shield increases your Armor by x.",
+    
+  }
+};
+// Major Universal Azerite Traits (T3)
+var traitsT3 = {
+}
+// Minor Azerite Traits (T2)
+var traitsT2 = {
+}
+// Defensive Azerite Traits (T1)
+var traitsT1 = {
+}
